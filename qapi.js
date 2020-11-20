025d5b0455f3f58ead110b30c0d62e2718ef76e6
tristanovacondio <info@tristanovacondio.com>
2020-11-20 16:08:28 +0100

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fb7db832c4ef2001e328c0c",
+				"5fb7db832c4ef2001e328c0e"
+			]
+		},
+		"5fb7db832c4ef2001e328c0c": {
+			"id": "5fb7db832c4ef2001e328c0c",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fb7db832c4ef2001e328c0e": {
+			"id": "5fb7db832c4ef2001e328c0e",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
