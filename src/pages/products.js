5f2fe0359d98aa56c069cab398ce95e8c931f5bc
tristanovacondio <info@tristanovacondio.com>
2020-11-20 20:43:29 +0100

    Changes

--- src/pages/products.js
+++ src/pages/products.js
@@ -19,7 +19,7 @@ export default (() => {
 						slot="StackItemContent"
 						margin-right="-20%"
 						padding-bottom="120%"
-						background="url(https://images.unsplash.com/photo-1552374196-c4e7ffc6e126?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80) 50% 15%/cover"
+						background="url(https://uploads.quarkly.io/5fb7db832c4ef2001e328c09/images/IMG_20201119_162007.jpg?v=2020-11-20T19:24:39.734Z) 50% 15%/cover"
 						lg-margin-right="0"
 						lg-padding-bottom="80%"
 						lg-background="url(https://uploads.quarkly.io/5fb7db832c4ef2001e328c09/images/IMG_20201119_162007.jpg?v=2020-11-20T19:24:39.734Z) 50% 15%/cover"
@@ -31,13 +31,14 @@ export default (() => {
 						padding="98px 64px"
 						margin-left="-20%"
 						margin-top="36px"
-						background="--color-red"
+						background="--color-greyD2"
 						color="--light"
-						mix-blend-mode="lighten"
+						mix-blend-mode="normal"
 						lg-margin-left="0"
 						lg-margin-top="0"
 						flex-direction="column"
 						lg-background="#464646"
+						opacity="50%"
 					/>
 					<Text
 						as="h4"
@@ -52,7 +53,7 @@ export default (() => {
 						Ha Ha Ha
 					</Text>
 					<Text as="p" margin="16px 0" font="--base" max-width="400px">
-						My name is David R. Moore, born and raised in France, worked as a professional photographer and videographer for more than 10 years, awarded the CDS Documentary Essay Prize in 2018 and Best Photographer of The Year 2019 by Sony World Photography Awards.
+						A simple and classic design
 					</Text>
 				</StackItem>
 			</Stack>
